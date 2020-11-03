5ff920661da9fca70d2167ada3c8b29a006493d5
Andrés Arias <arias9306@gmail.com>
2020-11-03 18:47:22 -0500

    feat: find by lesson id and create a lesson input

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,26 +1,20 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(LessonType)
 export class LessonResolver {
   constructor(private readonly lessonService: LessonService) {}
   @Query(() => LessonType)
-  lesson() {
-    return {
-      id: '123456',
-      name: 'Andres',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(() => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
-    return this.lessonService.createLessson(name, startDate, endDate);
+    return this.lessonService.createLessson(createLessonInput);
   }
 }
